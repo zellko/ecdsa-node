25f09e0ee55e88a1377ca31a93d998dd0c86ed3c
zellko <wyttenda@tutanota.com>
2022-12-30 22:17:30 +0100

    Added KeyPairs component to App and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
+import server from "./server";
 import Wallet from "./Wallet";
 import Transfer from "./Transfer";
+import KeysDisplay from "./KeysDisplay";
+
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+
+
 
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [keys, setKeys] = useState(undefined);
+
+  useEffect(()=>{
+    async function getKeys() {
+      const data = await server.get(`keys/`);
+
+      console.log(data.data)
+      setKeys(data.data)
+    }
+
+    getKeys();
+
+  }, [])
 
   return (
     <div className="app">
@@ -16,6 +35,7 @@ function App() {
         setAddress={setAddress}
       />
       <Transfer setBalance={setBalance} address={address} />
+      <KeysDisplay  keys={keys}/>
     </div>
   );
 }

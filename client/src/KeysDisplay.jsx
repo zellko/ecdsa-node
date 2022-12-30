cec538fec9b48bfaa39c69cd1492d24294f5eade
zellko <wyttenda@tutanota.com>
2022-12-30 22:16:13 +0100

    Created KeysDisplay component
    This component render a container which display public key / private key address

---
+++ client/src/KeysDisplay.jsx
@@ -0,0 +1,55 @@
+function copyKeyToClipboard(e) {
+    navigator.clipboard.writeText(e.target.textContent);
+}
+
+
+function KeysDisplay({ keys }) {
+
+    function renderKeysTable() {
+        if (keys === undefined){
+            return(
+                <p>Oops, something went wrong generating keys.</p>
+            )
+        }
+
+        const publicKeys = Object.keys(keys);
+
+        return(
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Public</th>
+                            <th>Private</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                          <td onClick={copyKeyToClipboard}>{publicKeys[0]}</td>
+                          <td onClick={copyKeyToClipboard}>{keys[publicKeys[0]]}</td>
+                        </tr>
+                        <tr>
+                          <td onClick={copyKeyToClipboard}>{publicKeys[1]}</td>
+                          <td onClick={copyKeyToClipboard}>{keys[publicKeys[1]]}</td>
+                        </tr>
+                        <tr>
+                          <td onClick={copyKeyToClipboard}>{publicKeys[2]}</td>
+                          <td onClick={copyKeyToClipboard}>{keys[publicKeys[2]]}</td>
+                        </tr>
+                  </tbody>
+                </table>
+        )   
+    }
+
+
+  return (
+    <div className="container keys">
+      <h1>Random Generated keys</h1>
+      <h2>⚠️ Thoses keys should not be used to generate any wallet! ⚠️</h2>
+    
+      {renderKeysTable()}
+      <p>Click on a keys to copy it to clipboard! </p>
+    </div>
+  );
+}
+
+export default KeysDisplay;

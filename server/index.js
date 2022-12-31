fbd4e61b3ee91026e8b843c5940040b768a513c0
zellko <wyttenda@tutanota.com>
2022-12-31 22:16:37 +0100

    Added server side verification of the transaction

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { json } = require("express");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
+
+/******
+Functions to generate random public / private keys pairs...
+... and to load them with virtual token.
+
+!!! For the purpose of this exercice only !!!
+******/
 
 function getAddress(publicKey) {
   // Get 0x usable address from the pub key
@@ -44,8 +53,6 @@ function setBalance() {
   for (let index = 0; index < addressList.length; index++) {
     balances[addressList[index]] = 100 - (index * 10);
   }
-
-  console.log(balances)
 }
 
 const keysPairs = {};
@@ -53,6 +60,46 @@ const balances = {};
 generateRandomPKey(); // Generate three keys pairs at the start of the server
 
 
+/******
+Functions to check broadcasted transaction of the network
+******/
+
+function hashMessage(message) {
+  const messageToBytes = utf8ToBytes(message);
+  return keccak256(messageToBytes); 
+}
+
+async function recoverKey(hash, signature, recoveryBit) {
+  const pubKey = await secp.recoverPublicKey(hash, signature, recoveryBit);
+  return pubKey;
+}
+
+async function checkSignature(signature, hash, publicKey) {
+  const isSigned = secp.verify(signature, hash, publicKey, {strict: true});
+  return isSigned;
+}
+
+/******
+Functions to convert data from object to uint8 array. 
+******/
+
+function convertObjectToArray(obj) {
+  let array = [];
+  const objKeys= Object.keys(obj);
+
+  for (let index = 0; index < objKeys.length; index++) {
+    const keys = objKeys[index];
+    const value = obj[keys];
+
+    array.push(value);
+  }
+
+  return new Uint8Array(array);;
+}
+
+/******
+Server (Network) listening functions 
+******/
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
@@ -65,12 +112,52 @@ app.get("/keys", (req, res) => {
   res.send(keysPairs);
 });
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+app.post("/send",async (req, res) => {
+  const { message, messageHash, signature } = req.body;
+  const { sender, recipient, amount } = message
+  const [sig, recoveryBit] = signature;
 
+  console.log("Network received transactions datas")
+
+  // Set balance if address are not part of the list
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
+  // Data are provided as object and must be converted back as uint8 array...
+  // ...to be usable with ethereum-cryptography library
+  let messageHashArray = convertObjectToArray(messageHash);
+  let sigArray = convertObjectToArray(sig);
+
+  // Server check if the message and message hash are the same. 
+  const messageHashToString = JSON.stringify(messageHashArray);
+  const messageToString = JSON.stringify(hashMessage(JSON.stringify(message)));
+
+  if ( messageHashToString !== messageToString){
+      res.status(400).send({ message: "Message and message hash are not matching!" });
+      return
+  }
+  console.log("Message and Hash are matching")
+
+  // Server check if the public address is matching sender address
+  const publicKey = await recoverKey(messageHashArray, sigArray, recoveryBit);
+  const publicKeyToAddress = getAddress(publicKey);
+
+  if ( sender !== publicKeyToAddress){
+    res.status(400).send({ message: "Sender addresse is not matching signed transaction address!" });
+    return
+  }
+
+  console.log("Pubkey and sender are matching")
+
+  // Server check if the signature is valid. Q: Is this check needed ?
+  const isSignatureValid = await checkSignature(sigArray, messageHashArray, publicKey);
+  if (!isSignatureValid){
+    res.status(400).send({ message: "Invalid Signature!" });
+    return
+  }
+  console.log("Signature is valid")
+
+  //Update balance
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
